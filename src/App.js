262bd7cd986bd673baf2125152e9891e83803773
Roshan Choxi <roshan.choxi@gmail.com>
2018-04-03 22:06:33 -0500

    add titles to board

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import "./App.css"
 export default class App extends React.Component {
   render() {
     return <div className="App">
+      <h3>Continuous Zoom</h3>
       <Board />
+      <h3>Discrete Zoom</h3>
       <Board zoomSnap={ true } />
     </div>
   }
