8fb6f918e9389b994d79c2d8ca687f8945b897a6
Roshan Choxi <roshan.choxi@gmail.com>
2018-04-03 19:15:12 -0500

    Grid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,68 @@
 import React from "react"
+import Hammer from "react-hammerjs"
+import "./App.css"
+
+class Note extends React.Component {
+  render() {
+    return <h1>Note</h1>
+  }
+}
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = { zoom: 1.0, log: [] }
+  }
+
+  handlePinch(event) {
+    this.print(event)
+    this.setState({ zoom: event.scale })
+  }
+
+  handleTap(event) {
+    this.print(event)
+  }
+
+  print(object) {
+    this.setState({ log: [...this.state.log, Object.assign({}, object)] })
+  }
+
+  renderLog() {
+    return <ul> 
+      { this.state.log.map(event => this.renderEvent(event)) }
+    </ul>
+  }
+
+  renderEvent(event) {
+    let parsedEvent = {
+      type: event.type,
+      deltaX: event.deltaX,
+      deltaY: event.deltaY,
+      scale: event.scale
+    }
+
+    return <li> { JSON.stringify(parsedEvent, null, 4) } </li>
+  }
+
   render() {
-    return <h1>hello world</h1>
+    let options = {
+      recognizers: {
+        pinch: { enable: true }
+      }
+    }
+
+    return <div className="App">
+      <Hammer 
+        options={ options } 
+        onPinch={ (...args) => this.handlePinch(...args) }
+        onTap={ (...args) => this.handleTap(...args) }
+      >
+        <div className="Board" style={{ zoom: this.state.zoom }}>
+          <Note />
+        </div>
+      </Hammer>
+
+      <div>Log: { this.renderLog() }</div>
+    </div>
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
+import TouchEmulator from "hammer-touchemulator"
+
+TouchEmulator()
 
 ReactDOM.render(<App />, document.getElementById("App"))
 
