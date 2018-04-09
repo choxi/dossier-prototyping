0ebb97de4cda9361e2f05e2913d27b3f1d252442
Roshan Choxi <roshan.choxi@gmail.com>
2018-04-09 12:30:29 -0500

    drag and drop multiple notes

--- src/playgrounds/Piles/Board.js
+++ src/playgrounds/Piles/Board.js
@@ -1,6 +1,8 @@
 import React from "react"
 import Note from "./Note"
 import Hammer from "react-hammerjs"
+import { List } from "immutable"
+
 import "./styles.css"
 
 export default class Board extends React.Component {
@@ -8,27 +10,33 @@ export default class Board extends React.Component {
     super()
 
     this.state = { 
-      notes: [
-        { id: 1, x: 0, y: 0, deltaX: 0, deltaY: 0 }
-      ] 
+      notes: List([
+        { id: 1, x: 0, y: 0, deltaX: 0, deltaY: 0 },
+        { id: 2, x: 100, y: 100, deltaX: 0, deltaY: 0 }
+      ]) 
     }
   }
 
   handlePan(note, event) {
-    console.log("handlePan")
+    let notes = this.state.notes
 
+    let index = notes.findIndex(n => n.id === note.id)
     let newNote = Object.assign({}, note, { deltaX: event.deltaX, deltaY: event.deltaY })
-    this.setState({ notes: [ newNote ]})
+    let newNotes = notes.set(index, newNote)
+
+    this.setState({ notes: newNotes })
   }
 
   handlePanEnd(note, event) {
-    console.log("handlePanEnd")
+    let notes = this.state.notes
+    let index = notes.findIndex(n => n.id === note.id)
 
     let newX = note.x + note.deltaX
     let newY = note.y + note.deltaY
-
     let newNote = Object.assign({}, note, { x: newX, y: newY, deltaX: 0, deltaY: 0 })
-    this.setState({ notes: [ newNote ] })
+    let newNotes = notes.set(index, newNote)
+
+    this.setState({ notes: newNotes })
   }
 
   render() {
