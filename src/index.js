935a64425c35096a1f72cc03dbbc76cfc4d00f63
Roshan Choxi <roshan.choxi@gmail.com>
2018-04-09 15:29:54 -0500

    move into index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import TouchEmulator from "hammer-touchemulator"
 
+document.addEventListener("touchmove", function(event){ event.preventDefault(); });
+
 TouchEmulator()
 
 ReactDOM.render(<App />, document.getElementById("App"))
