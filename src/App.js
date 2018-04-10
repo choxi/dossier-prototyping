458253e8ddce96dee78dc1393957df5c49ec1dcc
Roshan Choxi <roshan.choxi@gmail.com>
2018-04-10 12:31:22 -0500

    Make playgrounds browseable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react"
+import { BrowserRouter, Route, Link } from "react-router-dom"
+
 import Zoom from "./playgrounds/Zoom"
 import Piles from "./playgrounds/Piles"
+
 import "./App.scss"
 
 export default class App extends React.Component {
   render() {
-    return <div className="App">
-      <Piles />
-    </div>
+    return <BrowserRouter>
+      <div className="App">
+        <div className="App__navigation">
+          <ul>
+            <li><Link to="/piles">Piles</Link></li>
+            <li><Link to="/zoom">Zoom</Link></li>
+          </ul>
+        </div>
+
+        <div className="App__body">
+          <Route path="/piles" component={ Piles } />
+          <Route path="/zoom" component={ Zoom } />
+        </div>
+      </div>
+    </BrowserRouter>
   }
 }
