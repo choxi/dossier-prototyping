4a9cc30779bce30dc731ec6230e696bd7eafa614
Adam Wiggins <a@adamwiggins.com>
2018-04-12 20:01:48 +0200

    add notes to basic drinks

--- src/playgrounds/WhiskyAndGin/WhiskyAndGinBoard.js
+++ src/playgrounds/WhiskyAndGin/WhiskyAndGinBoard.js
@@ -19,13 +19,16 @@ export default class WhiskyAndGinBoard extends React.Component {
         { groupId: null, id: 1, x: 200, y: 10, deltaX: 0, deltaY: 0, text: "Whisky-based" },
         { groupId: null, id: 2, x: 700, y: 10, deltaX: 0, deltaY: 0, text: "Gin-based" },
         // drink images
-        { groupId: null, id: 3, x: 20, y: 60, deltaX: 0, deltaY: 0, imgSrc: "https://i.imgur.com/sYmDxKW.jpg" },
+        { groupId: 1, id: 3, x: 20, y: 60, deltaX: 0, deltaY: 0, imgSrc: "https://i.imgur.com/sYmDxKW.jpg" },
         { groupId: null, id: 4, x: 200, y: 480, deltaX: 0, deltaY: 0, imgSrc: "https://imgur.com/9AXtNyM.jpg" },
-        { groupId: null, id: 5, x: 300, y: 60, deltaX: 0, deltaY: 0, imgSrc: "https://i.imgur.com/3F1nPsq.jpg" },
+        { groupId: 2, id: 5, x: 300, y: 60, deltaX: 0, deltaY: 0, imgSrc: "https://i.imgur.com/3F1nPsq.jpg" },
         // recipe images
-        { groupId: null, id: 6, x: 20, y: 220, deltaX: 0, deltaY: 0, imgSrc: "https://i.imgur.com/04AqCJC.png" },
-        { groupId: null, id: 7, x: 300, y: 310, deltaX: 0, deltaY: 0, imgSrc: "https://i.imgur.com/CtpZwlp.png" },
-        { groupId: null, id: 8, x: 400, y: 550, deltaX: 0, deltaY: 0, imgSrc: "https://imgur.com/3tFOAGl.png" }
+        { groupId: 1, id: 6, x: 20, y: 220, deltaX: 0, deltaY: 0, imgSrc: "https://i.imgur.com/04AqCJC.png" },
+        { groupId: 2, id: 7, x: 300, y: 310, deltaX: 0, deltaY: 0, imgSrc: "https://i.imgur.com/CtpZwlp.png" },
+        { groupId: null, id: 8, x: 400, y: 550, deltaX: 0, deltaY: 0, imgSrc: "https://imgur.com/3tFOAGl.png" },
+        // notes
+        { groupId: 1, id: 9, x: 5, y: 320, deltaX: 0, deltaY: 0, imgSrc: "https://imgur.com/c0AZd5y.png" },
+        { groupId: null, id: 10, x: 700, y: 500, deltaX: 0, deltaY: 0, imgSrc: "https://imgur.com/0KKetF0.png" }
       ])
     }
 
